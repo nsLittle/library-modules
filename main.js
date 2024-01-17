b4c4ed9846010f3aca7baa262886cb2fc3ca9785
nsLittle <notsolittle88@gmail.com>
2024-01-17 09:40:28 -0500

    deleted erroneous space

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 // write your code here to make the tests pass
+const Library = function () {
+  let books = [];
+  let addBook = function (book) {
+    books.push(book);
+  };
+  let checkOutBook = function (book) {
+    if (books.includes(book)) {
+    book.setAttribute("checkedOut", true)
+    };
+  };
+  let returnBook = function (book) {
+    book.setAttribute("checkedOut", false)
+  };
+  return {
+    addBook: addBook,
+    checkOutBook: checkOutBook,
+    returnBook: returnBook
+  };
+};
+
+const Book = function () {
+  let attributes = {
+    checkedOut: false
+  };
+  let getAttribute = function (property) {
+    return attributes[property];
+  };
+  let setAttribute = function (property, value) {
+    attributes[property] = value;
+  };
+  return {
+    getAttribute: getAttribute,
+    setAttribute: setAttribute
+  }
+};
